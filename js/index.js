bd2474f7569cac030164b7c601a5b1c6baf14ffa
iamraihan <trueraihan@gmail.com>
2023-03-06 23:30:56 +0600

    update

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
-console.log("object");
+const heroSection = document.getElementById("hero-section");
+
+window.addEventListener("resize", () => {
+  const width = window.outerWidth;
+  if (width <= 992) {
+    heroSection.classList.add("container");
+  } else {
+    heroSection.classList.remove("container");
+  }
+});
